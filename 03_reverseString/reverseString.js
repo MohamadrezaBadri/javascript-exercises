866559b9cc4932e2c39dc8075bf25b03a899ea12
Mohamadreza Badri <bdry015@gmail.com>
2024-07-30 16:23:05 +0330

    Complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reverse = '' ; 
+    for (let i = text.length ; i > 0  ; i--){
+        reverse += text[i-1];
+    }
+    return reverse
 };
 
 // Do not edit below this line
