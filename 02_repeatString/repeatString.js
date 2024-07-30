aec601a42ffd103dd233d4b12fc163b2e5037e25
Mohamadreza Badri <bdry015@gmail.com>
2024-07-30 15:50:04 +0330

    Complete repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(text, num ) {
+  if (num == 0 ) {
+    return ""
+  }
+  if(num < 0 ) {
+    return "ERROR" 
+  }
+    let Message = text;
+    for (let i = 0; i < num -1; i++) {
+    Message += text;
+}
+return Message
 };
 
 // Do not edit below this line
