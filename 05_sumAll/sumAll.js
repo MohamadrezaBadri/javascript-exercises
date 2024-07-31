a1f52cdbcae75b2b4e6cd8cead2397ce70447423
Mohamadreza Badri <bdry015@gmail.com>
2024-07-31 12:10:04 +0330

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1 , num2) {
+let temp 
+let sum  = 0
+    if (num2  < num1){
+    temp = num2
+    num2 = num1
+    num1 = temp
+}
+if (num1 < 0 || Number.isInteger(num1) == false || Number.isInteger(num2) == false ){
+    return 'ERROR'
+}
+for (let i = num1 ; i<= num2 ; i++){
+    sum +=  i 
 
+}
+return sum
 };
 
 // Do not edit below this line
