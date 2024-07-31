ea876683f1e1292cc5a780cf1e588d1f58a4cce8
Mohamadreza Badri <bdry015@gmail.com>
2024-07-31 13:15:20 +0330

    Complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg) {
+  let cel = (deg - 32 )* 5/9 
+   return Number(cel.toFixed(1))  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+ let fahr =  (deg * 9/5 +32)
+  return Number(fahr.toFixed(1)) 
 };
 
 // Do not edit below this line
